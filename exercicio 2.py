a6f541994c3587ec35621c5cbcd51373412d0c97
Matheus-ux234 <matheusrsponline@hotmail.com>
2025-10-18 20:34:18 -0300

    Delete exercicio 2.py

--- exercicio 2.py
+++
@@ -1,8 +0,0 @@
-numchave=20
-
-num=float(input('Digite Um numero entre 0 e 100: '))
-if num<0 or num>100:
-    print('Numero invalido.')
-else:
-    dist= abs(numchave-num)
-    print(f'A distancia entre o numero chave e o numero {num} equivale a: {dist}')
